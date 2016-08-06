6e14c5771a1646c21faeda0d75cc12c286aab8f1
Fabian Stäber <fabian@fstab.de>
2016-08-06 09:48:24 +0200

    Suppress output of `create-route`

--- examples/start.sh
+++ examples/start.sh
@@ -139,7 +139,7 @@ echo -n "Creating default route... "
 aws ec2 create-route \
     --route-table-id "$ROUTE_TABLE_ID" \
     --destination-cidr-block "0.0.0.0/0" \
-    --gateway-id "$INTERNET_GATEWAY_ID"
+    --gateway-id "$INTERNET_GATEWAY_ID" > /dev/null
 
 echo done
 
